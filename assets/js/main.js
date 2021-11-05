c699ac2697b3b58dd3d8dc0cb6939e451b916a41
elgatofeliz <elgatofeliz@mail.de>
2021-11-05 17:46:36 +0100

    erste und letzte

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+function showMenu() {
+    document.getElementById("mobile").style.transform = "translateY(-0%)"
+    document.getElementById("mobile").style.transition = "45s ease"
+}
+
+function closeMenu() {
+    document.getElementById("mobile").style.transform = "translateY(100%)"
+    document.getElementById("mobile").style.transition = "15s ease"
+}
